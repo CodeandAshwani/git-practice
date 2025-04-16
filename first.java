69c2c87d7867b25f1dccf636e7e9ccadb0f13de0
CodeandAshwani <ashwanijha2005@gmail.com>
2025-04-16 20:14:46 +0530

    jj

--- first.java
+++ first.java
@@ -5,15 +5,15 @@ public class first
 public static void main(String args[])
 {
     
-int z, y, temp;
+int z, y, n;
 System.out.println("Enter z and y");
 Scanner sct = new Scanner(System.in);   
 z = sct.nextInt();  
 y = sct.nextInt();
 System.out.println("Before Swapping\nz = "+z+"\ny = "+y);
-temp = z;   
+n = z;   
 z = y;
-y = temp;
+y = n;
 System.out.println("After Swapping\nz = "+z+"\ny = "+y);
 }
 }
