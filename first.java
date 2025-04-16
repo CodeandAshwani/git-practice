7a06d228fb6b724108950bd67dbb77a1117f53a7
CodeandAshwani <ashwanijha2005@gmail.com>
2025-04-16 20:08:55 +0530

    seoc

--- first.java
+++ first.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 public class first
 {
+
 public static void main(String args[])
 {
+    
 int z, y, temp;
 System.out.println("Enter z and y");
-Scanner sct = new Scanner(System.in);   //User inputs two numbers
-z = sct.nextInt();   //User inputs two numbers
+Scanner sct = new Scanner(System.in);   
+z = sct.nextInt();  
 y = sct.nextInt();
 System.out.println("Before Swapping\nz = "+z+"\ny = "+y);
-temp = z;   //Swapping is done
+temp = z;   
 z = y;
 y = temp;
 System.out.println("After Swapping\nz = "+z+"\ny = "+y);
